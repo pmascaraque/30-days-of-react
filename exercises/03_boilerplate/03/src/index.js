e6cd57d35818afdf6acf7c5096db8e6e13c515ec
pmascaraque <ssm.pablo@gmail.com>
2021-04-27 15:25:28 +0100

    Added last exercise (Asabeneh portfolio) html and css style, day03 finished

--- exercises/03_boilerplate/03/src/index.js
+++ exercises/03_boilerplate/03/src/index.js
@@ -4,6 +4,7 @@ import html_logo from "./images/html_logo.png";
 import css_logo from "./images/css_logo.png";
 import react_logo from "./images/react_logo.png";
 import js_logo from "./images/js_logo.jpg";
+import asabeneh from "./images/asabeneh.jpg";
 import "./index.css"
 
 const app = (
@@ -29,7 +30,29 @@ const app = (
       <button>Subscribe</button>
     </div>
 
-    <footer></footer>
+    <footer>
+      <img id="sensei" src={asabeneh} alt="Asabeneh" />
+      <h1>ASABENEH YETAYEH</h1>
+      <p>Senior Developer, Finland</p>
+      <h2>SKILLS</h2>
+      <div id="skills">
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>Sass</p>
+        <p>JS</p>
+        <p>React</p>
+        <p>Redux</p>
+        <p>Node</p>
+        <p>MongoDB</p>
+        <p>Python</p>
+        <p>Flask</p>
+        <p>Django</p>
+        <p>MYSQL</p>
+        <p>GraphQL</p>
+        <p>D3.js</p>
+      </div>
+      <p>Joined on Aug 30, 2020</p>
+    </footer>
   </div>
 );
 

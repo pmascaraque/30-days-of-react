1fd6b4d3751c0a2fb2ee1a931baa286ea5678fd9
pmascaraque <ssm.pablo@gmail.com>
2021-04-27 14:29:19 +0100

    styled images in image container

--- exercises/03_boilerplate/03/src/index.js
+++ exercises/03_boilerplate/03/src/index.js
@@ -6,22 +6,27 @@ import react_logo from "./images/react_logo.png";
 import js_logo from "./images/js_logo.jpg";
 import "./index.css"
 
-const user = (
+const app = (
   <div className="App">
+
     <header>
       <h1>Front End Technologies</h1>
       <div id="w3_container">
-        <img src={html_logo} alt="HTML" />
-        <img src={css_logo} alt="CSS" />
-        <img src={js_logo} alt="Javascript" />
-        <img src={react_logo} alt="React" />
+        <img class="w3" src={html_logo} alt="HTML" />
+        <img class="w3" src={css_logo} alt="CSS" />
+        <img class="w3" src={js_logo} alt="Javascript" />
+        <img class="w3" src={react_logo} alt="React" />
       </div>
     </header>
-    <body></body>
+
+    <body>
+      
+    </body>
+
     <footer></footer>
   </div>
 );
 
 const rootElement = document.getElementById("root");
 // we render the JSX element using the ReactDOM package
-ReactDOM.render(user, rootElement);
+ReactDOM.render(app, rootElement);

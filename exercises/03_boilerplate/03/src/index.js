2b5eb4cfc1823809ad4dc159ae417b5c31063fae
pmascaraque <ssm.pablo@gmail.com>
2021-04-27 14:23:23 +0100

    added images into website

--- exercises/03_boilerplate/03/src/index.js
+++ exercises/03_boilerplate/03/src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import html_logo from "./images/html_logo.png";
+import css_logo from "./images/css_logo.png";
+import react_logo from "./images/react_logo.png";
+import js_logo from "./images/js_logo.jpg";
+import "./index.css"
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const user = (
+  <div className="App">
+    <header>
+      <h1>Front End Technologies</h1>
+      <div id="w3_container">
+        <img src={html_logo} alt="HTML" />
+        <img src={css_logo} alt="CSS" />
+        <img src={js_logo} alt="Javascript" />
+        <img src={react_logo} alt="React" />
+      </div>
+    </header>
+    <body></body>
+    <footer></footer>
+  </div>
 );
+
+const rootElement = document.getElementById("root");
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(user, rootElement);

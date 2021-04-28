1b9b06598cae0b64095d078b2fa0ca245f7ae79f
pmascaraque <ssm.pablo@gmail.com>
2021-04-28 08:52:27 +0100

    Organized HTML elements into react components

--- exercises/04_components/04/src/index.js
+++ exercises/04_components/04/src/index.js
@@ -5,56 +5,66 @@ import css_logo from "./images/css_logo.png";
 import react_logo from "./images/react_logo.png";
 import js_logo from "./images/js_logo.jpg";
 import asabeneh from "./images/asabeneh.jpg";
-import "./index.css"
+import "./index.css";
 
-const app = (
-  <div>
-    <header>
-      <h1>Front End Technologies</h1>
-      <div id="w3_container">
-        <img class="w3" src={html_logo} alt="HTML" />
-        <img class="w3" src={css_logo} alt="CSS" />
-        <img class="w3" src={js_logo} alt="Javascript" />
-        <img class="w3" src={react_logo} alt="React" />
-      </div>
-    </header>
-
-    <div id="main">
-      <h1>SUBSCRIBE</h1>
-      <p>Sign up with your email address to receive news and updates</p>
-      <form>
-        <input type="text" placeholder="First Name"></input>
-        <input type="text" placeholder="Last Name"></input>
-        <input type="text" placeholder="Email"></input>
-      </form>
-      <button>Subscribe</button>
+const Header = () => (
+  <header>
+    <h1>Front End Technologies</h1>
+    <div id="w3_container">
+      <img class="w3" src={html_logo} alt="HTML" />
+      <img class="w3" src={css_logo} alt="CSS" />
+      <img class="w3" src={js_logo} alt="Javascript" />
+      <img class="w3" src={react_logo} alt="React" />
     </div>
+  </header>
+);
 
-    <footer>
-      <img id="sensei" src={asabeneh} alt="Asabeneh" />
-      <h1>ASABENEH YETAYEH</h1>
-      <p>Senior Developer, Finland</p>
-      <h2>SKILLS</h2>
-      <div id="skills">
-        <p>HTML</p>
-        <p>CSS</p>
-        <p>Sass</p>
-        <p>JS</p>
-        <p>React</p>
-        <p>Redux</p>
-        <p>Node</p>
-        <p>MongoDB</p>
-        <p>Python</p>
-        <p>Flask</p>
-        <p>Django</p>
-        <p>MYSQL</p>
-        <p>GraphQL</p>
-        <p>D3.js</p>
-      </div>
-      <p>Joined on Aug 30, 2020</p>
-    </footer>
+const Main = () => (
+  <div id="main">
+    <h1>SUBSCRIBE</h1>
+    <p>Sign up with your email address to receive news and updates</p>
+    <form>
+      <input type="text" placeholder="First Name"></input>
+      <input type="text" placeholder="Last Name"></input>
+      <input type="text" placeholder="Email"></input>
+    </form>
+    <button>Subscribe</button>
+  </div>
+);
+
+const Footer = () => (
+  <footer>
+    <img id="sensei" src={asabeneh} alt="Asabeneh" />
+    <h1>ASABENEH YETAYEH</h1>
+    <p>Senior Developer, Finland</p>
+    <h2>SKILLS</h2>
+    <div id="skills">
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>Sass</p>
+      <p>JS</p>
+      <p>React</p>
+      <p>Redux</p>
+      <p>Node</p>
+      <p>MongoDB</p>
+      <p>Python</p>
+      <p>Flask</p>
+      <p>Django</p>
+      <p>MYSQL</p>
+      <p>GraphQL</p>
+      <p>D3.js</p>
+    </div>
+    <p>Joined on Aug 30, 2020</p>
+  </footer>
+);
+
+const App = () => (
+  <div className="app">
+    <Header />
+    <Main />
+    <Footer />
   </div>
 );
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(app, rootElement);
+ReactDOM.render(<App />, rootElement);

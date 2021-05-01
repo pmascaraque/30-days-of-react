78dbf5d8a71bb00cce5fe3ae4dcf580b4b238f5b
pmascaraque <ssm.pablo@gmail.com>
2021-05-01 12:42:05 +0100

    empty CRA

---
+++ exercises/08_states/08/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const app = (
+  <div>
+    <p>Hello</p>
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(app, rootElement);

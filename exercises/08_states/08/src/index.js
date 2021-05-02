89dd11fec0f71214a3628a9c1e0fc8e89030c60d
pmascaraque <ssm.pablo@gmail.com>
2021-05-02 08:10:25 +0100

    basic html

--- exercises/08_states/08/src/index.js
+++ exercises/08_states/08/src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import asabeneh from "./images/asabeneh.jpg";
 
 const app = (
   <div>
-    <p>Hello</p>
+    <div>
+      <h1>Welcome to 30 Days of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>Javascript Library</h3>
+      <h4>Asabeneh Yetayeh</h4>
+      <h5>Oct 7, 2020</h5>
+    </div>
+    
+    <div>
+      <p>Prerequisites to get started with React JS:</p>
+      <p>    HTML</p>
+      <p>    CSS</p>
+      <p>    JavaScript</p>
+      <img src={asabeneh} alt="Asabeneh" ></img>
+    </div>
   </div>
 );
 

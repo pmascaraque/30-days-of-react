c91d35636ad68951f1cd25cae3f5f43d97549774
pmascaraque <ssm.pablo@gmail.com>
2021-05-03 13:41:09 +0100

    Added button hook

--- exercises/08_states/08/src/index.js
+++ exercises/08_states/08/src/index.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import asabeneh from "./images/asabeneh.jpg";
 
+export default function Html() {
+  const [backGroundColor, setBackGroundColor] = useState("red");
+  return (
+    <div>
+      <div>
+        <h1>Welcome to 30 Days of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>Javascript Library</h3>
+        <h4>Asabeneh Yetayeh</h4>
+        <h5>Oct 7, 2020</h5>
+      </div>
+
+      <div>
+        <p>Prerequisites to get started with React JS:</p>
+        <p> HTML</p>
+        <p> CSS</p>
+        <p> JavaScript</p>
+        <img src={asabeneh} alt="Asabeneh"></img>
+        <h2>Asabeneh Yetayeh</h2>
+        <p className="blueButton">Greet People</p>
+        <p className="blueButton">Show Time</p>
+        <button className="blueButton" onClick={() => setBackGroundColor("blue")}>Change Background</button>
+        {console.log(backGroundColor)}
+      </div>
+    </div>
+  );
+}
+
 const app = (
   <div>
-    <div>
-      <h1>Welcome to 30 Days of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>Javascript Library</h3>
-      <h4>Asabeneh Yetayeh</h4>
-      <h5>Oct 7, 2020</h5>
-    </div>
-    
-    <div>
-      <p>Prerequisites to get started with React JS:</p>
-      <p>    HTML</p>
-      <p>    CSS</p>
-      <p>    JavaScript</p>
-      <img src={asabeneh} alt="Asabeneh" ></img>
-      <h2>Asabeneh Yetayeh</h2>
-      <p className="blueButton">Greet People</p>
-      <p className="blueButton">Show Time</p>
-      <p className="blueButton">Change Background</p>
-    </div>
+    <Html />
   </div>
 );
 

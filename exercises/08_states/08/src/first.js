a99f282a0534ec4bbca479cd57f23d1d5c49f13c
pmascaraque <ssm.pablo@gmail.com>
2021-05-03 15:28:00 +0100

    changed order of files

---
+++ exercises/08_states/08/src/first.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+export default function Html() {
+  const [backGroundColor, setBackGroundColor] = useState("white");
+
+  const colors = ["Aqua", "DarkOliveGreen", "DarkRed", "Indigo"];
+
+  const greetPeople = () =>
+    alert("Welcome to 30 days of React Challenge, 2021");
+
+  const showDate = (time) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const month = months[time.getMonth()].slice(0, 3);
+    const year = time.getFullYear();
+    const date = time.getDate();
+    return ` ${month} ${date}, ${year}`;
+  };
+
+  const handleTime = () => {
+    alert(showDate(new Date()));
+  };
+
+  return (
+    <div style={{ backgroundColor: backGroundColor, height: 700 }}>
+      <button onClick={greetPeople}>Greet People</button>
+
+      <button onClick={handleTime}>Show Time</button>
+
+      <button
+        onClick={() =>
+          setBackGroundColor(colors[Math.floor(Math.random() * 4)])
+        }
+      >
+        Change Background
+      </button>
+    </div>
+  );
+}
+
+const app = (
+  <div>
+    <Html />
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(app, rootElement);

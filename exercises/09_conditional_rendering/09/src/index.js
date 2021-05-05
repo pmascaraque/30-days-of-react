fe17f49bc19fad05b128bff28904f0857d0c894f
pmascaraque <ssm.pablo@gmail.com>
2021-05-05 14:16:12 +0100

    added ternary and && rendering

--- exercises/09_conditional_rendering/09/src/index.js
+++ exercises/09_conditional_rendering/09/src/index.js
@@ -7,13 +7,20 @@ const App = () => {
 
   let status = boolean ? <h3>Bool is true</h3> : <h3>Bool is false</h3>;
 
+  let one = <h1>TRUE</h1>
+  let two = <h1>FALSE</h1>
+
+  let test = <h2>TEST appears on true</h2>
+
   const changeBoolean = () => setBoolean(!boolean);
 
   return (
     <div>
       Yello
+      {boolean ? one : two}
       {status}
       <button onClick={changeBoolean}>Change Bool</button>
+      {boolean && test}
     </div>
   );
 };

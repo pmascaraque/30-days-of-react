208ae55533fc2bfaf8d5255536bd846b07bab73c
pmascaraque <ssm.pablo@gmail.com>
2021-05-05 19:17:47 +0100

    added background by season

--- exercises/09_conditional_rendering/09/src/index.js
+++ exercises/09_conditional_rendering/09/src/index.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 
 const App = () => {
 
+  const getSeasonColor = () => {
+    let div_1_color;
+
+    switch (new Date().getMonth) {
+      case (0, 1, 2):
+        div_1_color = "white";
+        break;
+      case (3, 4, 5):
+        div_1_color = "green";
+        break;
+      case (6, 7, 8):
+        div_1_color = "yellow";
+        break;
+      case (9, 10, 11):
+        div_1_color = "brown";
+        break;
+      default:
+        div_1_color = "green";
+    }
+    return (div_1_color);
+  };
+
   return (
     <div>
-      Yello
+      <div style={{ backgroundColor: getSeasonColor(), height: 300 }}></div>
+      <div style={{ backgroundColor: "blue", height: 300 }}></div>
     </div>
   );
 };

5b4307a108c9d840a03de8774a4216ed0ac080ea
pmascaraque <ssm.pablo@gmail.com>
2021-05-05 20:49:57 +0100

    prettier

--- exercises/09_conditional_rendering/09/src/functions/GetTimeColor.js
+++ exercises/09_conditional_rendering/09/src/functions/GetTimeColor.js
@@ -1,10 +1,8 @@
 const getTimeColor = () => {
-    const hour = new Date().getHours();
+  const hour = new Date().getHours();
 
-    if (hour < 12 && hour >= 6)
-        return "blue"
-    if (hour < 18 && hour >= 12)
-        return "brown"
-    return "black"
-  };
-  export default getTimeColor();
+  if (hour < 12 && hour >= 6) return "blue";
+  if (hour < 18 && hour >= 12) return "brown";
+  return "black";
+};
+export default getTimeColor();

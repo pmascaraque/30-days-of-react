6486b38cd837cf837526a6b663b8026ccc9e67ab
pmascaraque <ssm.pablo@gmail.com>
2021-05-05 14:17:23 +0100

    cleaned up index.js to start exercises

--- exercises/09_conditional_rendering/09/src/index.js
+++ exercises/09_conditional_rendering/09/src/index.js
@@ -3,24 +3,10 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 
 const App = () => {
-  const [boolean, setBoolean] = useState(true);
-
-  let status = boolean ? <h3>Bool is true</h3> : <h3>Bool is false</h3>;
-
-  let one = <h1>TRUE</h1>
-  let two = <h1>FALSE</h1>
-
-  let test = <h2>TEST appears on true</h2>
-
-  const changeBoolean = () => setBoolean(!boolean);
 
   return (
     <div>
       Yello
-      {boolean ? one : two}
-      {status}
-      <button onClick={changeBoolean}>Change Bool</button>
-      {boolean && test}
     </div>
   );
 };

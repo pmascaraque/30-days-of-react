7a7a4e1cefe7f7016f3debd3da72b723b652891b
pmascaraque <ssm.pablo@gmail.com>
2021-05-05 14:08:33 +0100

    changed render function into ternary

--- exercises/09_conditional_rendering/09/src/index.js
+++ exercises/09_conditional_rendering/09/src/index.js
@@ -4,10 +4,8 @@ import "./styles/index.css";
 
 const App = () => {
   const [boolean, setBoolean] = useState(true);
-  let status;
 
-  if (boolean) status = <h3>Bool is true</h3>;
-  else status = <h3>Bool is false</h3>;
+  let status = boolean ? <h3>Bool is true</h3> : <h3>Bool is false</h3>;
 
   const changeBoolean = () => setBoolean(!boolean);
 

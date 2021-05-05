fc82fa176fc9dbe212e27a96c10a38e9429c04ee
pmascaraque <ssm.pablo@gmail.com>
2021-05-05 14:05:40 +0100

    added hook to change render onClick

--- exercises/09_conditional_rendering/09/src/index.js
+++ exercises/09_conditional_rendering/09/src/index.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 
 const App = () => {
+  const [boolean, setBoolean] = useState(true);
   let status;
 
-  let bool = false;
-
-  if (bool) status = <h3>Bool is true</h3>;
+  if (boolean) status = <h3>Bool is true</h3>;
   else status = <h3>Bool is false</h3>;
+
+  const changeBoolean = () => setBoolean(!boolean);
 
   return (
     <div>
       Yello
       {status}
+      <button onClick={changeBoolean}>Change Bool</button>
     </div>
   );
 };

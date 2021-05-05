0f1b86bdc196e333e6708e67f15c1ee5ff1e2782
pmascaraque <ssm.pablo@gmail.com>
2021-05-05 13:56:08 +0100

    basic conditional rendering

--- exercises/09_conditional_rendering/09/src/index.js
+++ exercises/09_conditional_rendering/09/src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./styles/index.css";
 
-const App = () => (
-  <div>
-  Yello
-</div>
-)
- 
+const App = () => {
+  let status;
+
+  let bool = false;
+
+  if (bool) status = <h3>Bool is true</h3>;
+  else status = <h3>Bool is false</h3>;
+
+  return (
+    <div>
+      Yello
+      {status}
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);

6e56eeae1e81fee07fa79858539ec93894323380
pmascaraque <ssm.pablo@gmail.com>
2021-05-05 19:58:02 +0100

    put functions into own file

---
+++ exercises/09_conditional_rendering/09/src/functions/GetSeasonColor.js
@@ -0,0 +1,23 @@
+const getSeasonColor = () => {
+  switch (new Date().getMonth()) {
+    case 0:
+    case 1:
+    case 2:
+      return "white";
+    case 3:
+    case 4:
+    case 5:
+      return "blue";
+    case 6:
+    case 7:
+    case 8:
+      return "yellow";
+    case 9:
+    case 10:
+    case 11:
+      return "brown";
+    default:
+      return "orange";
+  }
+};
+export default getSeasonColor();
--- exercises/09_conditional_rendering/09/src/index.js
+++ exercises/09_conditional_rendering/09/src/index.js
@@ -1,33 +1,12 @@
 import ReactDOM from "react-dom";
 import "./styles/index.css";
+import getSeasonColor from "./functions/GetSeasonColor"
 
 const App = () => {
-  const getSeasonColor = () => {
-    switch (new Date().getMonth()) {
-      case 0:
-      case 1:
-      case 2:
-        return "white";
-      case 3:
-      case 4:
-      case 5:
-        return "blue";
-      case 6:
-      case 7:
-      case 8:
-        return "yellow";
-      case 9:
-      case 10:
-      case 11:
-        return "brown";
-      default:
-        return "orange";
-    }
-  };
 
   return (
     <div>
-      <h1 style={{ backgroundColor: getSeasonColor(), height: 300 }}>
+      <h1 style={{ backgroundColor: getSeasonColor, height: 300 }}>
         Winter: White | Spring: Green | Summer: Yellow | Autumn: Brown
       </h1>
       <h1 style={{ backgroundColor: "blue", height: 300 }}>

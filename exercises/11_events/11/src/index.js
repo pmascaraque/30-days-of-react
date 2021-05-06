1c26ce7794ae111916aa8b93b9ca02213972208c
pmascaraque <ssm.pablo@gmail.com>
2021-05-06 18:17:50 +0200

    basic CRA

---
+++ exercises/11_events/11/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  <div>Yeello</div>
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);

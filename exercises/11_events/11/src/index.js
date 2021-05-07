b5c46412966d24b8f5544c0a930087d5d9e13b8d
pmascaraque <ssm.pablo@gmail.com>
2021-05-07 17:05:48 +0200

    removed 11

--- exercises/11_events/11/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  <div>Yeello</div>
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
